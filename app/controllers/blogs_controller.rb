7c8347dfa7558fe2c944717a69fd3470d8d089ba
kenmoi <ken.en001.mb@gmail.com>
2021-06-26 22:46:51 +0000

    Learning check

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,8 @@
 class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
+    p "標準出力にのみ反映"
+    logger.debug("標準出力とログファイルに記録される")
   end
 
   def show
@@ -13,8 +15,9 @@ class BlogsController < ApplicationController
 
   def create
     blog = Blog.new(blog_params)
+    binding.pry
     blog.save
-    redirect_to blogs_path(blog.id)
+    redirect_to blog_path(blog.id)
   end
 
   def edit
